06216d6d9ec8f33c3e8ac7489b07e455aa298e58
hasan <hasanalghanim@gmail.com>
2021-07-15 01:07:25 -0600

    Navbar Component
    Navbar Component

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 function NavBar() {
-  return <div>hello</div>;
+  return (
+    <div className="navBar">
+      <ul className="nav1">
+        <span>LOGO</span>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Top Movies</a>
+        </li>
+        <li>
+          <a href="#">Popular</a>
+        </li>
+      </ul>
+      <ul className="nav2">
+        <li>
+          <a href="#">Search</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default NavBar;
